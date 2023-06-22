324ff5921b0dfa8b1ba6fc1f3a1597be7d5ec819
Syed.Faraaz.Ali <Syed.Faraaz.Ali@skillassure.com>
2023-06-22 17:53:08 +0530

    Updated

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
 
-function add(e) {
-  e.preventDefault();
+var display = document.getElementById('display');
+var add_more_fields = document.getElementById('add_more_fields');
+var remove_fields = document.getElementById('remove_fields');
+
+add_more_fields.onclick = function(){
+    var input = display.getElementsByTagName('th');
+    var subid = input.length -3;
+
+    var newcold = document.createElement('th');
+    var newup = document.createTextNode("Subject " +subid);
+   
+    newcold.appendChild(newup);
+    display.appendChild(newcold);
+
+   
+        var newcol = document.createElement('td');
+        var newField = document.createElement('input');
+        newField.setAttribute('type','number');
+        newField.setAttribute('name','details[]');
+        newField.setAttribute('class','details');
+        newField.setAttribute('max','100');
+        newField.setAttribute('onInput','getvalue()');
+        newField.setAttribute('siz',10);
+        newField.setAttribute('placeholder','Enter Marks(Max=100)');
+        newcol.appendChild(newField);
+        details.appendChild(newcol);
+        
+        
+      console.log(input.length)
+    
+  }
+
+  remove_fields.onclick = function(){
+   
+    var input = display.getElementsByTagName('th');
+    var input2 = details.getElementsByTagName('td')
+    if(input.length > 4) {
+        display.removeChild(input[(input.length) - 1]);
+        details.removeChild(input2[(input2.length) - 1]);
+    }
+  }
+
+  function getvalue() {
+
+    var sum = 0;
+    var tags = details.getElementsByTagName('input');
+    for(var i =4;i<tags.length;i++)
+    {
+     sum = sum+parseInt(tags[i].value);
+    }
+    var n = parseInt(tags.length-4);
+    var avg= sum/n;
+    console.log(tags[3].value)
+    var val=0;
+    val=tags[3].value;
+    tags[3].value = avg;
+    
+}
+
+function postdata() {
+  
 
   const id= document.getElementById('id').value;
    const name = document.getElementById('name').value;
    const grade = document.getElementById('grade').value;
    const avg = document.getElementById('avg').value;
-
+ console.log(avg)
    const obj = {
        id,
        name,
@@ -14,8 +73,7 @@ function add(e) {
        avg
    }
 
-  
-   axios.post("https://crudcrud.com/api/65c9ca25f1dd437bbaeabe73d56878c7/data"
+   axios.post("https://crudcrud.com/api/38a9b0d045b54020b90552dc5ae48727/data"
    , obj).then((response)=>
    {
     console.log(response)
@@ -25,99 +83,30 @@ function add(e) {
    })
   }
 
+  getdata.onclick = function() {
+    fetch('https://crudcrud.com/api/38a9b0d045b54020b90552dc5ae48727/data')
+    .then(res => {
+      return res.json();
+    })
+    .then(data=> {
+      
+    
+       var table = "";
+       data.map((user)=>{
+        if(user.avg>60)
+        table+= `
+        
+        <tr>
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.grade}</td>
+        <td>${user.avg}</td>
+        </tr>`;
+       })
+      document.getElementById('tb').innerHTML=table;
+    })
+    
+     }
 
-
-var survey_options = document.getElementById('survey_options');
-var add_more_fields = document.getElementById('add_more_fields');
-var remove_fields = document.getElementById('remove_fields');
-
-const i=0;
-
-add_more_fields.onclick = function(){
-  var newField = document.createElement('input');
-  newField.setAttribute('type','number');
-  newField.setAttribute('name','survey_options[]');
-  newField.setAttribute('class','survey_options');
-  newField.setAttribute('max','100');
-  newField.setAttribute('onInput','getvalue()');
-  newField.setAttribute('siz',50);
-  newField.setAttribute('placeholder','Enter Subject Marks (Max=100)');
-  survey_options.appendChild(newField);
   
-}
-
-remove_fields.onclick = function(){
-  var input_tags = survey_options.getElementsByTagName('input');
-  if(input_tags.length > 3) {
-    survey_options.removeChild(input_tags[(input_tags.length) - 1]);
-  }
-}
-
-console.log(grade.value);
-
-sgrade.addEventListener('change', showgrade);
-function showgrade() {
-  grade.value = sgrade.value;
-}
-
-// function avg(e) {
-
-//   e.preventDefault();
-//   var sum = 0;
-//   var input_tags = survey_options.getElementsByTagName('input');
-//   for(var i =3;i<input_tags.length;i++)
-//   {
-//    sum = sum+parseInt(input_tags[i].value);
-//   }
-//   var n = parseInt(input_tags.length-3);
-//   var avg= sum/n;
-//   console.log(avg);
-// var avgvalue = document.getElementById('avg').value;
-
-// calc.avg.value = avg;
-//   console.log(calc.avg.value);
-
-//  }
-
- function getvalue() {
-
-  var sum = 0;
-  var input_tags = survey_options.getElementsByTagName('input');
-  for(var i =3;i<input_tags.length;i++)
-  {
-   sum = sum+parseInt(input_tags[i].value);
-  }
-  var n = parseInt(input_tags.length-3);
-  var avg= sum/n;
-  console.log(avg);
-var avgvalue = document.getElementById('avg').value;
-
-calc.avg.value = avg;
-   calc.avg.innerText = avg;
-
- }
-
-
- function get() {
-fetch('https://crudcrud.com/api/65c9ca25f1dd437bbaeabe73d56878c7/data')
-.then(res => {
-  return res.json();
-})
-.then(data=> {
-  
-
-   var table = "";
-   data.map((user)=>{
-    if(user.avg>60)
-    table+= ` <tr>
-    <td>${user.id}</td>
-    <td>${user.name}</td>
-    <td>${user.grade}</td>
-    <td>${user.avg}</td>
-    </tr>`;
-   })
-  document.getElementById('tb').innerHTML=table;
-})
-
- }
-
+   
